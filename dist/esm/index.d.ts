a268425d18ac44dc2631a339a659c846b1b29082
Tim Rogers <me@timrogers.co.uk>
2024-01-24 10:57:05 +0000

    feat: switch to the latest version of the InPost API
    BREAKING CHANGE: This updates changes the interface of the library,
    dropping the now-redundant `getAvailabilityForLocation` function
    and changing the shape of objects returned by existing functions.
    
    This change gets the library working again with the current version
    of the InPost private API, as used on InPost's website.
    
    As part of these changes:
    
    - The `getAvailabilityForLocation` function is removed, as other
      functions now return locker availability without an extra call.
    - The way that locker availability is described is changed. We now
      expose simple keys for availability (e.g.
      `smallLockerAvailability`) and availability is presented through
      the `LockerAvailabilityLevel` enum, rather than a specific number
      of free lockers.

--- dist/esm/index.d.ts
+++ dist/esm/index.d.ts
@@ -1,7 +1,6 @@
 import { ResponseError } from './errors';
-import { Location, LockerSize, LocationAvailability } from './types';
-export { ResponseError, LockerSize, Location, LocationAvailability };
-export declare const getAvailabilityForLocation: (locationId: string) => Promise<LocationAvailability>;
+import { Location, LockerSize, LockerAvailabilityLevel, ListedLocation } from './types';
+export { ResponseError, LockerSize, Location, LockerAvailabilityLevel, ListedLocation };
 export declare const getLocation: (locationId: string) => Promise<Location>;
-export declare const findLocationsByCoordinates: (latitude: number, longitude: number) => Promise<Location[]>;
-export declare const findLocationsByPostcode: (postcode: string) => Promise<Location[]>;
+export declare const findLocationsByCoordinates: (latitude: number, longitude: number) => Promise<ListedLocation[]>;
+export declare const findLocationsByPostcode: (postcode: string) => Promise<ListedLocation[]>;
